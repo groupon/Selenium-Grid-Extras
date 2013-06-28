cc3c8821ac8560f86c7acb93e5ba7946dfbe7850
Dima Kovalenko <dima@groupon.com>
2013-06-28 10:04:38 -0400

    Adding /api end point integration tests

---
+++ SeleniumGridExtras/src/test/specs/get_api_endpoints.rb
@@ -0,0 +1,52 @@
+# Copyright (c) 2013, Groupon, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# Neither the name of GROUPON nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+require File.expand_path('spec_helper', File.dirname(__FILE__))
+
+describe "ApiDocumentation.java" do
+  
+  before(:all) do  
+    @response = get_json "api"
+  end
+  
+  it "should have more than 0 records" do
+    @response.size.should > 0
+  end
+  
+  it "should have all records response_type be JSON" do
+    non_json = @response.collect {|r| r if r["response_type"] != "json"}.compact
+    non_json.empty?.should == true
+  end
+  
+  it "should not have any response with null response_descriptions" do
+    no_description = @response.collect {|r| r if r["response_description"].empty?}.compact
+    no_description.empty?.should == true
+  end
+end
