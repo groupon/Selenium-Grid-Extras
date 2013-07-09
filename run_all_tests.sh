cc230b9d373fb11e169a3290991b10b21eef2818
Dima Kovalenko <dima@groupon.com>
2013-07-09 15:06:56 -0400

    Adding run_all_tests.sh

---
+++ run_all_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+curl http://localhost:3000/stop_extras?confirm=true
+
+set -e
+
+rm -rf tmp
+mkdir tmp
+
+mvn package
+java -jar SeleniumGridExtras/target/SeleniumGridExtras-1.1.8-SNAPSHOT-jar-with-dependencies.jar > tmp/grid_log.txt &
+
+rvm gemset use grid_extras --create
+
+gem install rspec json
+
+rspec SeleniumGridExtras/src/test/specs/
+
+curl http://localhost:3000/stop_extras?confirm=true
