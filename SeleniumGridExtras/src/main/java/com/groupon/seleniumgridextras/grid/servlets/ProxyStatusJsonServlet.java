37e5ba43608ce5897baea2459c7a55f5823b5db3
Dima Kovalenko <dima@groupon.com>
2013-07-30 14:30:54 -0400

    - Change content type to application/json - Make version check for null so no crash

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/grid/servlets/ProxyStatusJsonServlet.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/grid/servlets/ProxyStatusJsonServlet.java
@@ -18,72 +18,79 @@ import java.util.Iterator;
 
 public class ProxyStatusJsonServlet extends RegistryBasedServlet {
 
-    private static final long serialVersionUID = -1975392591408983229L;
+  private static final long serialVersionUID = -1975392591408983229L;
 
-    public ProxyStatusJsonServlet() {
-        this(null);
+  public ProxyStatusJsonServlet() {
+    this(null);
+  }
+
+  public ProxyStatusJsonServlet(Registry registry) {
+    super(registry);
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    doPost(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    process(req, resp);
+
+  }
+
+  protected void process(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    response.setStatus(200);
+    JSONObject res;
+    try {
+      res = getResponse();
+      response.getWriter().print(res.toString(4));
+      response.getWriter().close();
+    } catch (JSONException e) {
+      throw new GridException(e.getMessage());
     }
 
-    public ProxyStatusJsonServlet(Registry registry) {
-        super(registry);
-    }
+  }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
-    }
+  private JSONObject getResponse() throws IOException, JSONException {
+    JSONObject requestJSON = new JSONObject();
+    ProxySet proxies = this.getRegistry().getAllProxies();
+    Iterator<RemoteProxy> iterator = proxies.iterator();
+    JSONArray Proxies = new JSONArray();
+    JSONArray ProxyStatus = new JSONArray();
+    while (iterator.hasNext()) {
+      RemoteProxy eachProxy = iterator.next();
+      Iterator<TestSlot> proxyIterator = eachProxy.getTestSlots().iterator();
+      while (proxyIterator.hasNext()) {
+        JSONObject TestMachine = new JSONObject();
+        TestSlot eachSlot = proxyIterator.next();
+        //getSession is null if not being used
+        if (eachSlot.getSession() == null) {
+          //System.out.println(eachSlot.getCapabilities().get("browserName").toString());
+          TestMachine.put("browserName", eachSlot.getCapabilities().get("browserName").toString());
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        process(req, resp);
+          String version = "null";
+          if (eachSlot.getCapabilities().containsKey("version")) {
+            version = eachSlot.getCapabilities().get("version").toString();
+          }
 
-    }
-
-    protected void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/json");
-        response.setCharacterEncoding("UTF-8");
-        response.setStatus(200);
-        JSONObject res;
-        try {
-            res = getResponse();
-            response.getWriter().print(res.toString(4));
-            response.getWriter().close();
-        } catch (JSONException e) {
-            throw new GridException(e.getMessage());
+          TestMachine.put("version", version);
         }
+        ProxyStatus.put(TestMachine);
+      }
 
+      Proxies.put(eachProxy.getOriginalRegistrationRequest().getAssociatedJSON());
     }
+    requestJSON.put("Proxies", ProxyStatus);
+    requestJSON.put("TotalProxies", Proxies);
 
-    private JSONObject getResponse() throws IOException, JSONException {
-        JSONObject requestJSON = new JSONObject();
-        ProxySet proxies = this.getRegistry().getAllProxies();
-        Iterator<RemoteProxy> iterator = proxies.iterator();
-        JSONArray Proxies = new JSONArray();
-        JSONArray ProxyStatus = new JSONArray();
-        while (iterator.hasNext()) {
-            RemoteProxy eachProxy = iterator.next();
-            Iterator<TestSlot> proxyIterator = eachProxy.getTestSlots().iterator();
-            while(proxyIterator.hasNext())
-            {
-                JSONObject TestMachine = new JSONObject();
-                TestSlot eachSlot = proxyIterator.next();
-                //getSession is null if not being used
-                if(eachSlot.getSession()==null)
-                {
-                    //System.out.println(eachSlot.getCapabilities().get("browserName").toString());
-                    TestMachine.put("browserName",eachSlot.getCapabilities().get("browserName").toString());
-                    TestMachine.put("version",eachSlot.getCapabilities().get("version").toString());
-                }
-                ProxyStatus.put(TestMachine);
-            }
-
-            Proxies.put(eachProxy.getOriginalRegistrationRequest().getAssociatedJSON());
-        }
-        requestJSON.put("Proxies",ProxyStatus);
-        requestJSON.put("TotalProxies",Proxies);
-
-        return requestJSON;
-    }
+    return requestJSON;
+  }
 
 }
 
