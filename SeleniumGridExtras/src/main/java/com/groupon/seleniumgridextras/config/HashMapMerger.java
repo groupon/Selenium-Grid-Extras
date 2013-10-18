aa7a0410d777ef8a42c95b4803e5d616719959f3
Dima Kovalenko <dima@groupon.com>
2013-10-18 12:13:40 -0400

    Delete files that will not be used, simplify the class to bare bones

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/HashMapMerger.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/HashMapMerger.java
@@ -1,32 +1,8 @@
 package com.groupon.seleniumgridextras.config;
 
-import com.google.gson.Gson;
-
-import java.util.HashMap;
 import java.util.Map;
 
 public class HashMapMerger {
-
-  public static Config merge(Config defaultConfig, Config overwriteConfig){
-
-    Map<String, Object> defaultConfigHash   = new HashMap();
-    defaultConfigHash.putAll(convertToHash(convertToString(defaultConfig)));
-
-    Map<String, Object> overwriteConfigHash = convertToHash(convertToString(overwriteConfig));
-    Map<String, Object> finalConfig = overwriteMergeStrategy(defaultConfigHash, overwriteConfigHash);
-
-    String finalConfigString = new Gson().toJson(finalConfig);
-
-    return new Gson().fromJson(finalConfigString, Config.class);
-  }
-
-  private static Map convertToHash(String json){
-    return new Gson().fromJson(json, HashMap.class);
-  }
-
-  private static String convertToString(Config config){
-    return config.toPrettyJsonString();
-  }
 
   protected static Map overwriteMergeStrategy(Map<String, Object> left, Map<String, Object> right){
     //As desribed in http://grepcode.com/file/repo1.maven.org/maven2/org.codehaus.cargo/cargo-core-api-module/0.9/org/codehaus/cargo/module/merge/strategy/MergeStrategy.java
