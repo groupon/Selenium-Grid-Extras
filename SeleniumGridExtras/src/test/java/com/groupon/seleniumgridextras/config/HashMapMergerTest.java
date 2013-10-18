8162650bf69173ab4f244d3005539e1c1d0cf122
Dima Kovalenko <dima@groupon.com>
2013-10-18 11:28:30 -0400

    Finish writing the most complicated test to date

--- SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/config/HashMapMergerTest.java
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/config/HashMapMergerTest.java
@@ -7,10 +7,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class HashMapMergerTest {
 
-  private String emptyConfigJson;
   private Map leftHash;
 
   @Before
@@ -41,6 +41,11 @@ public class HashMapMergerTest {
 
   @Test
   public void testMergeNestedMap() throws Exception {
+    //By far the most complicated test in here, i wish assertEquals would not freak out
+    //About having a nested array and just compared the values of the array and not the objects
+    //Don't try to understand this part unless you had 8 hour of sleep
+    //And feel free to fix it with something better!
+
 
     Map right = new HashMap();
 
@@ -64,8 +69,53 @@ public class HashMapMergerTest {
     Map actual = HashMapMerger.overwriteMergeStrategy(leftHash, right);
 
 
+    Map expected = new HashMap();
 
-    assertEquals("", actual);
+    expected.put("a", 1);
+    expected.put("b", "string");
+
+    String[] array1 = new String[2];
+    array1[0] = "a";
+    array1[1] = "b";
+    expected.put("c", array1);
+
+    Map expectedHashLevel1 = new HashMap();
+    expectedHashLevel1.put("aa", 2);
+    expectedHashLevel1.put("bb", "string bb");
+
+    Map expectedHashLevel2 = new HashMap();
+    expectedHashLevel2.put("aaa", "aaa");
+    expectedHashLevel2.put("bbb", 10);
+
+    String[] expectedStringArray = new String[2];
+    expectedStringArray[0] = "nested";
+    expectedStringArray[1] = "array";
+    expectedHashLevel2.put("ccc", expectedStringArray);
+
+    expectedHashLevel1.put("cc", expectedHashLevel2);
+
+
+    expected.put("d", expectedHashLevel1);
+
+    assertEquals(expected.keySet(), actual.keySet());
+    assertEquals(expected.get("a"), actual.get("a"));
+    assertEquals(expected.get("b"), actual.get("b"));
+    assertArrayEquals((String[]) expected.get("c"), (String[]) actual.get("c"));
+
+    Map expectedD = (HashMap) expected.get("d");
+    Map actualD   = (HashMap) actual.get("d");
+
+    assertEquals(expectedD.keySet(), actualD.keySet());
+    assertEquals(expectedD.get("aa"), actualD.get("aa"));
+    assertEquals(expectedD.get("bb"), actualD.get("bb"));
+
+    Map expectedCC = (HashMap) expectedD.get("cc");
+    Map actualCC   = (HashMap) actualD.get("cc");
+
+    assertEquals(expectedCC.keySet(), actualCC.keySet());
+    assertEquals(expectedCC.get("aaa"), actualCC.get("aaa"));
+    assertEquals(expectedCC.get("bbb"), actualCC.get("bbb"));
+    assertArrayEquals((String[]) expectedCC.get("ccc"), (String[]) actualCC.get("ccc"));
   }
 
   @Test
