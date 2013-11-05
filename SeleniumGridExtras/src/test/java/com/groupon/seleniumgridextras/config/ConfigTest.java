830281774e65d14f02d842353f20529b49badecf
Dima Kovalenko <dima@groupon.com>
2013-11-05 09:34:16 -0500

    start writing configTEst.java

---
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/config/ConfigTest.java
@@ -0,0 +1,38 @@
+package com.groupon.seleniumgridextras.config;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ConfigTest {
+
+  Config config;
+  String filename = "config_test.json";
+
+  @Before
+  public void setUp() throws Exception {
+    config = DefaultConfig.getDefaultConfig();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    File f = new File(filename);
+
+    if (f.exists()){
+      f.delete();
+    }
+  }
+
+  @Test
+  public void testGetWebdriver() throws Exception {
+
+    config.writeToDisk(filename);
+
+
+  }
+}
