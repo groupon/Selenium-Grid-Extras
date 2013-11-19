a68405649b7c7f278c5bdc4621628f19eb397af1
Dima Kovalenko <dima@groupon.com>
2013-11-19 16:30:50 -0500

    Clean up Daemon interface

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/daemons/Daemon.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/daemons/Daemon.java
@@ -1,21 +1,6 @@
 package com.groupon.seleniumgridextras.daemons;
 
 public interface Daemon {
-
   public Boolean startDaemon();
-
   public Boolean stopDaemon();
-
-  public void setExecutable(String executable);
-
-  public void setLogDir(String dir);
-
-  public String getInitDFilePath();
-
-  public void setDaemonName(String name);
-
-  public void setWorkingDir(String dir);
-
-  public void setJava(String java);
-
 }
