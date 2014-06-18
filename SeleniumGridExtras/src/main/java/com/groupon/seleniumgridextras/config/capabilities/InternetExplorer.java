bdf496b286274dcc6bc3236d752dcf97daaada1d
Dima Kovalenko <dima@groupon.com>
2014-06-18 15:51:23 -0400

    Explicitly set max instances for Internet Explorer to 1

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/capabilities/InternetExplorer.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/capabilities/InternetExplorer.java
@@ -1,3 +1,11 @@
 package com.groupon.seleniumgridextras.config.capabilities;
 
-public class InternetExplorer extends Capability {}
+public class InternetExplorer extends Capability {
+
+  public InternetExplorer() {
+    this.put("maxInstances", 1);
+    this.put("seleniumProtocol", "WebDriver");
+    setBrowser(getWDStyleName());
+  }
+
+}
