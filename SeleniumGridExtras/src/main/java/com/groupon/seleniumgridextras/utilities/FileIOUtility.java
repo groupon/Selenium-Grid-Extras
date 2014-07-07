50a8b549110e10a217adee789989a6a4ec4f0892
Dima Kovalenko <dima@groupon.com>
2014-07-07 16:27:31 -0400

    More specific errors

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/FileIOUtility.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/FileIOUtility.java
@@ -5,7 +5,9 @@ import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA. User: dima Date: 7/1/14 Time: 3:27 PM To change this template use
@@ -15,18 +17,24 @@ public class FileIOUtility {
 
   private static Logger logger = Logger.getLogger(FileIOUtility.class);
 
-  public static String getAsString(String file) throws Exception {
+  public static String getAsString(String file) throws FileNotFoundException {
     return getAsString(new File(file));
   }
 
 
-  public static String getAsString(File file) throws Exception {
+  public static String getAsString(File file) throws FileNotFoundException{
 
     String readString = "";
     BufferedReader reader = new BufferedReader(new FileReader(file));
     String line = null;
+
+    try{
     while ((line = reader.readLine()) != null) {
       readString = readString + line;
+    }
+    }catch (IOException error){
+      logger.error("IOExcetion reading "+ file.getAbsolutePath());
+      logger.error(error);
     }
 
     logger.debug("Read from" + file.getAbsolutePath() + " following content\n" + readString);
