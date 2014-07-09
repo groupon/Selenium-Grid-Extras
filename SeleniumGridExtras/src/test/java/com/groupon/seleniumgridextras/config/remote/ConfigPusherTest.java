c829089b195ce12c9c5cdea9789706a3d8795e0c
Dima Kovalenko <dima@groupon.com>
2014-07-09 12:12:39 -0400

    Delete all HTTP tests, they live now in HttpUtilityTEsts

--- SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/config/remote/ConfigPusherTest.java
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/config/remote/ConfigPusherTest.java
@@ -1,10 +1,13 @@
-package com.groupon.seleniumgridextras.config;
+package com.groupon.seleniumgridextras.config.remote;
 
+import com.groupon.seleniumgridextras.config.RuntimeConfig;
+import com.groupon.seleniumgridextras.config.remote.ConfigPusher;
 import com.groupon.seleniumgridextras.utilities.FileIOUtility;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.utils.URIBuilder;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -38,39 +41,12 @@ public class ConfigPusherTest {
   @Test
   public void testBasicParam() throws Exception {
 
-    assertEquals("http://host:3000/update_node_config?node=" +
-                 RuntimeConfig.getOS().getHostName()
-                 + "&filename=file&content=content", config.buildUrl("file", "content").toString());
+    Assert.assertEquals("http://host:3000/update_node_config?node=" +
+                        RuntimeConfig.getOS().getHostName()
+                        + "&filename=file&content=content",
+                        config.buildUrl("file", "content").toString());
   }
 
-
-  @Test
-  public void testSendRequest() throws Exception{
-    URI uri = new URIBuilder()
-        .setScheme("http")
-        .setHost("google.com")
-        .setPort(80)
-        .setPath("/")
-        .build();
-
-    HttpResponse actual = config.sendRequest(uri);
-
-    assertEquals(200, actual.getStatusLine().getStatusCode());
-  }
-
-  @Test
-  public void testBadUrlSend() throws Exception{
-    URI uri = new URIBuilder()
-        .setScheme("http")
-        .setHost("localhost")
-        .setPort(8888)
-        .setPath("/")
-        .build();
-
-    HttpResponse actual = config.sendRequest(uri);
-
-    assertEquals(404, actual.getStatusLine().getStatusCode());
-  }
 
   @Test
   public void testConfigFileReader() throws Exception {
