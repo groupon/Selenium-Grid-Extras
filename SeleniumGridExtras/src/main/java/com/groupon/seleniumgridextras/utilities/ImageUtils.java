0561342ebfb0f7608dfa6b896f8fe24f6081f02a
Dima Kovalenko <dima@groupon.com>
2014-08-26 17:08:00 -0400

    Addint a utility to convert images to and from base64

---
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/ImageUtils.java
@@ -0,0 +1,62 @@
+package com.groupon.seleniumgridextras.utilities;
+
+import java.io.IOException;
+
+import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class ImageUtils {
+
+  /**
+   * Decode string to image
+   * @param imageString The string to decode
+   * @return decoded image
+   */
+  public static BufferedImage decodeToImage(String imageString) {
+
+    BufferedImage image = null;
+    byte[] imageByte;
+    try {
+      BASE64Decoder decoder = new BASE64Decoder();
+      imageByte = decoder.decodeBuffer(imageString);
+      ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+
+      image = ImageIO.read(bis);
+      bis.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return image;
+  }
+
+  /**
+   * Encode image to string
+   * @param image The image to encode
+   * @param type jpeg, bmp, ...
+   * @return encoded string
+   */
+  public static String encodeToString(BufferedImage image, String type) {
+    String imageString = null;
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+    try {
+      ImageIO.write(image, type, bos);
+      byte[] imageBytes = bos.toByteArray();
+
+      BASE64Encoder encoder = new BASE64Encoder();
+      imageString = encoder.encode(imageBytes);
+
+      bos.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return imageString;
+  }
+
+}
