243dc9d947a9cfdd70a8a1bca0b7a227cd7d2d13
Dima Kovalenko <dima@groupon.com>
2014-08-27 13:45:58 -0400

    Updating caption generator to be more dynamic with image sizes

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/videorecording/ImageProcessor.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/videorecording/ImageProcessor.java
@@ -1,28 +1,44 @@
 package com.groupon.seleniumgridextras.videorecording;
 
+import com.groupon.seleniumgridextras.utilities.ImageUtils;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ImageProcessor {
 
 
-  public static BufferedImage addTextCaption(BufferedImage image, String line1, String line2, String line3, String line4){
+  public static BufferedImage addTextCaption(BufferedImage image, String line1, String line2,
+                                             String line3, String line4) {
 
-      Graphics g = image.getGraphics();
+    Graphics g = image.getGraphics();
 
-      g.setColor(new Color(129, 182, 64, 128)); //Set the rectangle color to Groupon green and transperent
-      g.fillRect(0, 700, 1024, 52); //Add rectangle at the very bottom of the screen
+    final int imageHeight = image.getHeight();
+    final int imageWidth = image.getWidth();
+    final int borderHeight = 52;
+    final int firstLineStartHeight = imageHeight - borderHeight;
+    final int textLeftStartPosition = 40;
 
 
+    g.setColor(
+        new Color(129, 182, 64, 128)); //Set the rectangle color to Groupon green and transperent
 
-      g.setColor(Color.white); //Set text font color
-      g.setFont(g.getFont().deriveFont(12f)); //Set text size
+    g.fillRect(0, firstLineStartHeight, imageWidth,
+               borderHeight); //Add rectangle at the very bottom of the screen
 
-      g.drawString(line1, 10, 714);
-      g.drawString(line2, 10, 726);
-      g.drawString(line3, 10, 738);
-      g.drawString(line4, 10, 750);
-      g.dispose();
+    g.setColor(Color.white); //Set text font color
+    g.setFont(g.getFont().deriveFont(12f)); //Set text size
+
+
+    g.drawString(line1, textLeftStartPosition, firstLineStartHeight + 12);
+    g.drawString(line2, textLeftStartPosition, firstLineStartHeight + 24);
+    g.drawString(line3, textLeftStartPosition, firstLineStartHeight + 36);
+    g.drawString(line4, textLeftStartPosition, firstLineStartHeight + 48);
+    g.dispose();
 
     return image;
 
--- SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/videorecording/ImageProcessorTest.java
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/videorecording/ImageProcessorTest.java
@@ -16,19 +16,25 @@ import static org.junit.Assert.assertEquals;
  */
 public class ImageProcessorTest {
 
+
   @Test
   public void testAddTextCaptions() throws Exception {
 
     BufferedImage processedImage = MissingFrameImage.getMissingFrameAsBufferedImage();
+
     processedImage = ImageProcessor.addTextCaption(processedImage,
-                                           "LINE 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                                           "LINE 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                                           "LINE 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                                           "LINE 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+                                                   "LINE 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                   "LINE 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                   "LINE 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                   "LINE 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+
+    // Uncomment this line after updating ImageProcessor class, to generate a new expected image
+    // ImageUtils.saveImage(new File("SeleniumGridExtras/src/test/resources/fixtures/expected_processed_image.png"), processedImage);
 
     String actual = ImageUtils.encodeToString(processedImage, "PNG");
-
-    File expectedFile = new File(ClassLoader.getSystemResource("fixtures/expected_processed_image.png").getFile());
+    final File
+        expectedFile =
+        new File(ClassLoader.getSystemResource("fixtures/expected_processed_image.png").getFile());
 
     assertEquals(ImageUtils.encodeToString(ImageUtils.readImage(expectedFile), "PNG"), actual);
   }
