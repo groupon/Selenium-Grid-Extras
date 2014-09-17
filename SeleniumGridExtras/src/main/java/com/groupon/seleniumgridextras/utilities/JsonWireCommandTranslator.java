44733779f753ec5a640941290bd1e9a1035d6b2d
Dima Kovalenko <dima@groupon.com>
2014-09-17 13:15:21 -0400

    Adding a translator of command URL to human readable command

---
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/JsonWireCommandTranslator.java
@@ -0,0 +1,97 @@
+package com.groupon.seleniumgridextras.utilities;
+
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.UnsupportedCommandException;
+import org.openqa.selenium.remote.Command;
+import org.openqa.selenium.remote.SessionId;
+import org.openqa.selenium.remote.http.HttpMethod;
+import org.openqa.selenium.remote.http.HttpRequest;
+import org.openqa.selenium.remote.http.JsonHttpCommandCodec;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class JsonWireCommandTranslator {
+  private static Logger logger = Logger.getLogger(JsonWireCommandTranslator.class);
+  private String body;
+  private String method;
+  private String url;
+
+  public JsonWireCommandTranslator(String method, String url, String body) {
+    this.method = method;
+    this.body = body;
+    this.url = url.replaceAll("/wd/hub", "");
+  }
+
+  public String toString() {
+    JsonHttpCommandCodec codec = new JsonHttpCommandCodec();
+    Command translatedCommand;
+    try{
+    translatedCommand = codec.decode(getRequest());
+    } catch (UnsupportedCommandException e){
+      translatedCommand = new Command(new SessionId(""), this.url);
+    }
+    return translatedCommand.getName() + ": " + getBody();
+  }
+
+  public HttpRequest getRequest(){
+    return new HttpRequest(getMethod(), url);
+  }
+
+  public String getBody(){
+    return this.body;
+  }
+
+  public HttpMethod getMethod() {
+    if (this.method.equals("POST")) {
+      return HttpMethod.POST;
+    } else if (this.method.equals("GET")) {
+      return HttpMethod.GET;
+    } else {
+      return HttpMethod.DELETE;
+    }
+  }
+
+  public static String getBodyAsString(HttpServletRequest request){
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader bufferedReader = null;
+
+    try {
+      InputStream inputStream = request.getInputStream();
+      if (inputStream != null) {
+        bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        char[] charBuffer = new char[128];
+        int bytesRead = -1;
+        while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+          stringBuilder.append(charBuffer, 0, bytesRead);
+        }
+      } else {
+        stringBuilder.append("");
+      }
+    } catch (IOException ex) {
+      stringBuilder.append("Error: Could not parse body anymore");
+      logger.warn("Error parsing body of request");
+      logger.warn(stringBuilder.toString());
+      logger.warn(ex);
+    } finally {
+      if (bufferedReader != null) {
+        try {
+          bufferedReader.close();
+        } catch (IOException ex) {
+          stringBuilder.append("Error closing body reader");
+          logger.warn("Error closing body reader");
+          logger.warn(ex);
+        }
+      }
+    }
+
+    return stringBuilder.toString();
+
+  }
+
+}
---
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/utilities/JsonWireCommandTranslatorTest.java
@@ -0,0 +1,50 @@
+package com.groupon.seleniumgridextras.utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JsonWireCommandTranslatorTest {
+
+  private static final String POST = "POST";
+
+
+  @Test
+  public void testNewSessionCommand() throws Exception {
+    String url = "/session";
+    String
+        body =
+        "{\"desiredCapabilities\":{\"platform\":\"ANY\",\"javascriptEnabled\":true,\"cssSelectorsEnabled\":true,\"browserName\":\"firefox\",\"nativeEvents\":false,\"rotatable\":false,\"takesScreenshot\":true,\"version\":\"\"}}";
+
+    assertEquals("newSession: " + body, new JsonWireCommandTranslator(POST, url, body).toString());
+
+  }
+
+  @Test
+  public void testFindElement() throws Exception {
+    String url = "/wd/hub/session/a4e9aff3-2d78-4394-b9ff-0883232244d7/element";
+    String body = "{\"using\":\"id\",\"value\":\"gbqfq\"}";
+
+    assertEquals("findElement: " + body, new JsonWireCommandTranslator(POST, url, body).toString());
+  }
+
+  @Test
+  public void testGetCommand() throws Exception {
+    String url = "/wd/hub/session/a4e9aff3-2d78-4394-b9ff-0883232244d7/url";
+    String body = "{\"url\":\"http://google.com\"}";
+
+    assertEquals("get: " + body, new JsonWireCommandTranslator(POST, url, body).toString());
+  }
+
+  @Test
+  public void testUnknownCommand() throws Exception {
+    String url = "/session/a4e9aff3-2d78-4394-b9ff-0883232244d7/lalalalalalalala";
+    String body = "{\"url\":\"http://google.com\"}";
+
+    assertEquals(url + ": " + body, new JsonWireCommandTranslator(POST, url, body).toString());
+  }
+
+
+}
