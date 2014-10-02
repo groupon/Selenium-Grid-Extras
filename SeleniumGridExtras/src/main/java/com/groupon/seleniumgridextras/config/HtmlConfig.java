8e5790a9b6cdca66d044a247ecde0c23838f22c1
Dima Kovalenko <dima@groupon.com>
2014-10-02 16:11:03 -0400

    Since we use a jar, we cannot use a File for packaged resources

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/HtmlConfig.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/HtmlConfig.java
@@ -14,6 +14,10 @@ public class HtmlConfig extends HashMap {
   public static final String MAIN_JS_FALLBACK = "main_js_fallback";
   public static final String MAIN_TEMPLATE = "main_template";
   public static final String MAIN_TEMPLATE_FALLBACK = "main_template_fallback";
+  public static final String HTML_HEADER_FILE = "html_header_file";
+  public static final String HTML_NAV_BAR = "html_nav_bar";
+  public static final String HTML_FOOTER_PARTIAL = "html_footer_partial";
+
 
   public void setMainCss(String cssFile) {
     this.put(PRIMARY_CSS_FILE, cssFile);
@@ -31,56 +35,78 @@ public class HtmlConfig extends HashMap {
     this.put(JQUERY_FALLBACK, jsUrl);
   }
 
-  public void setMainJs(String jsFile){
+  public void setMainJs(String jsFile) {
     this.put(MAIN_JS_FILE, jsFile);
   }
 
-  public void setMainJsFallBack(String jsUrl){
+  public void setMainJsFallBack(String jsUrl) {
     this.put(MAIN_JS_FALLBACK, jsUrl);
   }
 
-  public void setTemplateJs(String jsFile){
+  public void setTemplateJs(String jsFile) {
     this.put(MAIN_TEMPLATE, jsFile);
   }
 
-  public void setTemplateJsFallback(String jsUrl){
+  public void setTemplateJsFallback(String jsUrl) {
     this.put(MAIN_TEMPLATE_FALLBACK, jsUrl);
   }
 
-  //Here
+  public void setHtmlHeadFile(String file) {
+    this.put(HTML_HEADER_FILE, file);
+  }
 
-  public File getMainCss() {
-    return new File((String) this.get(PRIMARY_CSS_FILE));
+  public String getHtmlHeadFile() {
+    return (String) this.get(HTML_HEADER_FILE);
+  }
+
+  public void setHtmlNavBar(String file){
+    this.put(HTML_NAV_BAR, file);
+  }
+
+  public String getHtmlNavBar(){
+    return (String) this.get(HTML_NAV_BAR);
+  }
+
+  public void setHtmlFooter(String file){
+    this.put(HTML_FOOTER_PARTIAL, file);
+  }
+
+  public String getHtmlFooter(){
+    return (String) this.get(HTML_FOOTER_PARTIAL);
+  }
+
+
+  public String getMainCss() {
+    return (String) this.get(PRIMARY_CSS_FILE);
   }
 
   public String getMainCssFallBack() {
     return (String) this.get(PRIMARY_CSS_FILE_FALLBACK);
   }
 
-  public File getJquery() {
-    return new File((String) this.get(JQUERY_FILE));
+  public String getJquery() {
+    return (String) this.get(JQUERY_FILE);
   }
 
   public String getJqueryFallBack() {
     return (String) this.get(JQUERY_FALLBACK);
   }
 
-  public File getMainJs(){
-    return new File((String) this.get(MAIN_JS_FILE));
+  public String getMainJs() {
+    return (String) this.get(MAIN_JS_FILE);
   }
 
-  public String getMainJsFallBack(){
+  public String getMainJsFallBack() {
     return (String) this.get(MAIN_JS_FALLBACK);
   }
 
-  public File getTemplateSource(){
-    return new File((String) this.get(MAIN_TEMPLATE));
+  public String getTemplateSource() {
+    return (String) this.get(MAIN_TEMPLATE);
   }
 
-  public String getTemplateJsFallback(){
+  public String getTemplateJsFallback() {
     return (String) this.get(MAIN_TEMPLATE_FALLBACK);
   }
-
 
 
 }
