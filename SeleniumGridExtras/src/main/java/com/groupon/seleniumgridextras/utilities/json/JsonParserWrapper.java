f848ce0e9e67b4dd13cf2aa243e4d98258302279
Dima Kovalenko <dima@groupon.com>
2014-10-02 11:35:11 -0400

    Adding an overwrite class to convert JsonObjects into plain maps

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/json/JsonParserWrapper.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/json/JsonParserWrapper.java
@@ -2,15 +2,21 @@ package com.groupon.seleniumgridextras.utilities.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonParserWrapper {
 
-  public static Map stringToMap(String input) {
+  public static Map toHashMap(String input) {
     return new Gson().fromJson(input, HashMap.class);
   }
+
+  public static Map toHashMap(JsonObject input) {
+    return new Gson().fromJson(input, HashMap.class);
+  }
+
 
   public static String mapToPrettyPrintJson(Map input) {
     return new GsonBuilder().setPrettyPrinting().create().toJson(input);
