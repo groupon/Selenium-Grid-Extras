b4b4012190260ef1f0a7824e8fc2ea3b50f0bab9
Dima Kovalenko <dima@groupon.com>
2014-10-02 16:09:59 -0400

    Use apache utility instead of the home baged thing

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/StreamUtility.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/StreamUtility.java
@@ -1,22 +1,14 @@
 package com.groupon.seleniumgridextras.utilities;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-/**
- * Created with IntelliJ IDEA. User: dima Date: 7/8/14 Time: 3:49 PM To change this template use
- * File | Settings | File Templates.
- */
 public class StreamUtility {
 
   public static String inputStreamToString(InputStream is) throws IOException {
-    StringBuilder result = new StringBuilder();
-    int in;
-    while ((in = is.read()) != -1) {
-      result.append((char) in);
-    }
-    is.close();
-    return result.toString();
+    return IOUtils.toString(is, "UTF-8");
   }
 
 }
