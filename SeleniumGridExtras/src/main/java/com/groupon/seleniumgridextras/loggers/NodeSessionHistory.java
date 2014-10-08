a0bfeedaf648ed2e50fe1c4b0895537647f7e49f
Dima Kovalenko <dima@groupon.com>
2014-10-08 16:52:12 -0400

    Adding object to keep track of sessions for a given computer node

---
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/loggers/NodeSessionHistory.java
@@ -0,0 +1,61 @@
+package com.groupon.seleniumgridextras.loggers;
+
+import com.groupon.seleniumgridextras.utilities.FileIOUtility;
+import com.groupon.seleniumgridextras.utilities.TimeStampUtility;
+import com.groupon.seleniumgridextras.utilities.json.JsonParserWrapper;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class NodeSessionHistory {
+
+    protected Date created;
+    protected List<Map> sessions;
+    protected File outputFile;
+    protected long logRotationDuration = 86400000; //24 hours
+
+    private static Logger logger = Logger.getLogger(NodeSessionHistory.class);
+
+    public NodeSessionHistory(Date nodeNameTimestamp, File outputFile) {
+        this.created = nodeNameTimestamp;
+        this.sessions = new LinkedList<Map>();
+        this.outputFile = outputFile;
+    }
+
+    public void addNewSession(Map sessionInfo){
+        this.sessions.add(sessionInfo);
+    }
+
+    public List<Map> getSessions(){
+        return sessions;
+    }
+
+    public String toJson(){
+        return JsonParserWrapper.prettyPrintString(sessions);
+    }
+
+    public void backupToFile(){
+        try {
+            FileIOUtility.writePrettyJsonToFile(outputFile, sessions);
+        } catch (IOException e) {
+            logger.warn("Unable to backup session history to file for " + outputFile.getAbsolutePath());
+            logger.warn(e);
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public boolean timeToRotateLog() {
+        //Rotate log every 24 hours
+        return (TimeStampUtility.getTimestamp().getTime() - this.created.getTime() > logRotationDuration);
+    }
+
+    public void setLogRotationDuration(long ms){
+        this.logRotationDuration = ms;
+    }
+
+}
---
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/loggers/NodeSessionHistoryTest.java
@@ -0,0 +1,90 @@
+package com.groupon.seleniumgridextras.loggers;
+
+import com.groupon.seleniumgridextras.utilities.FileIOUtility;
+import com.groupon.seleniumgridextras.utilities.TimeStampUtility;
+import com.groupon.seleniumgridextras.utilities.json.JsonParserWrapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class NodeSessionHistoryTest {
+
+    private NodeSessionHistory history;
+    private Date timestamp;
+    private File outputFile = new File("session_output_log.json");
+    private Map expectedSession;
+    private final String expectedJson =
+            "[\n" +
+                    "  {\n" +
+                    "    \"id\": \"123456\",\n" +
+                    "    \"time\": \"time\"\n" +
+                    "  }\n" +
+                    "]";
+
+    @Before
+    public void setUp() throws Exception {
+        deleteOutput();
+        timestamp = TimeStampUtility.getTimestamp();
+        history = new NodeSessionHistory(timestamp, outputFile);
+
+        expectedSession = new HashMap();
+        expectedSession.put("id", "123456");
+        expectedSession.put("time", "time");
+
+        history.addNewSession(expectedSession);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        deleteOutput();
+    }
+
+    @Test
+    public void testAddNewSession() throws Exception {
+        assertEquals(1, history.getSessions().size());
+        assertEquals(expectedSession, history.getSessions().get(0));
+    }
+
+    @Test
+    public void testGetSessionsAsJson() throws Exception {
+        assertEquals(expectedJson, history.toJson());
+    }
+
+
+    @Test
+    public void testBackupToFile() throws Exception {
+        deleteOutput();
+        history.backupToFile();
+
+        assertTrue(outputFile.exists());
+
+        List actual = JsonParserWrapper.toList(FileIOUtility.getAsString(outputFile));
+        assertEquals(1, actual.size());
+        assertEquals(expectedSession, actual.get(0));
+
+    }
+
+    @Test
+    public void testTimeToRotateLog() throws Exception {
+        NodeSessionHistory timedHistory = new NodeSessionHistory(timestamp, outputFile);
+
+        assertFalse(timedHistory.timeToRotateLog());
+        timedHistory.setLogRotationDuration(1000);
+        Thread.sleep(1500);
+        assertTrue(timedHistory.timeToRotateLog());
+    }
+
+    private void deleteOutput() {
+        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+    }
+}
