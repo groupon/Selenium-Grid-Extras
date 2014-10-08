dd91b5800dfa84dfd5adbdb88fa33cec5c47f932
Dima Kovalenko <dima@groupon.com>
2014-10-08 16:33:52 -0400

    new timestamp return formatting

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/TimeStampUtility.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/TimeStampUtility.java
@@ -1,20 +1,22 @@
 package com.groupon.seleniumgridextras.utilities;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- * Created with IntelliJ IDEA. User: dima Date: 9/17/14 Time: 4:40 PM To change this template use
- * File | Settings | File Templates.
- */
+
 public class TimeStampUtility {
 
-  public static Date getTimestamp(){
-    return new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
-  }
+    public static Date getTimestamp() {
+        return new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
+    }
 
-  public static String getTimestampAsString(){
-    return getTimestamp().toString();
-  }
+    public static String getTimestampAsString() {
+        return getTimestamp().toString();
+    }
+
+    public static String osFriendlyTimestamp() {
+        return new SimpleDateFormat("dd_MM_yyyy").format(getTimestamp());
+    }
 
 }
