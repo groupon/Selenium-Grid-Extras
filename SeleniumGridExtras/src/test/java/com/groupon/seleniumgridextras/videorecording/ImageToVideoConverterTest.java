5a8a37254c9e82dcef029b7069529923740c4fd4
Dima Kovalenko <dima@groupon.com>
2014-10-08 13:44:01 -0400

    Missed one test that has a video component

--- SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/videorecording/ImageToVideoConverterTest.java
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/videorecording/ImageToVideoConverterTest.java
@@ -1,5 +1,6 @@
 package com.groupon.seleniumgridextras.videorecording;
 
+import com.groupon.seleniumgridextras.config.RuntimeConfig;
 import org.junit.After;
 import org.junit.Test;
 
@@ -13,45 +14,45 @@ import static org.junit.Assert.assertTrue;
 
 public class ImageToVideoConverterTest {
 
-  final private File outputVideo = new File("1.mp4");
-  final private
-  String
-      inputDirt =
-      ClassLoader.getSystemResource("fixtures/videoscreenshots").getFile();
+    final private File outputVideo = new File("1.mp4");
+    final private
+    String
+            inputDirt =
+            ClassLoader.getSystemResource("fixtures/videoscreenshots").getFile();
 
-  @After
-  public void tearDown() throws Exception {
-    if (outputVideo.exists()){
-      outputVideo.delete();
-    }
-  }
-
-  @Test
-  public void testVideoConverter() throws Exception {
-
-    ImageToVideoConverter
-        converter =
-        new ImageToVideoConverter(inputDirt, "1", "localhost", "Today");
-
-    ExecutorService cachedPool = Executors.newCachedThreadPool();
-    try {
-
-      Future<String> future = cachedPool.submit(converter);
-
-
-
-      String result = future.get();
-
-      assertEquals("done", result);
-      assertEquals(true, outputVideo.exists());
-      assertTrue(outputVideo.length() > 20000); //Can't check video frame by frame but know it's
-                                                //roughly 200K when properly formatted
-
-    } finally {
-      cachedPool.shutdown();
+    @After
+    public void tearDown() throws Exception {
+        if (outputVideo.exists()) {
+            outputVideo.delete();
+        }
     }
 
-  }
+    @Test
+    public void testVideoConverter() throws Exception {
+        if (RuntimeConfig.getOS().hasGUI()) {
+            ImageToVideoConverter
+                    converter =
+                    new ImageToVideoConverter(inputDirt, "1", "localhost", "Today");
 
+            ExecutorService cachedPool = Executors.newCachedThreadPool();
+            try {
+
+                Future<String> future = cachedPool.submit(converter);
+
+
+                String result = future.get();
+
+                assertEquals("done", result);
+                assertEquals(true, outputVideo.exists());
+                assertTrue(outputVideo.length() > 20000); //Can't check video frame by frame but know it's
+                //roughly 200K when properly formatted
+
+            } finally {
+                cachedPool.shutdown();
+            }
+
+        }
+
+    }
 
 }
