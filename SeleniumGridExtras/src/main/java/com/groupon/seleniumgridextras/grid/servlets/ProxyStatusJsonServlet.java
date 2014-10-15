ad30253350979b7cba8f90c56ce995cd00d4e2a9
Dima Kovalenko <dima@groupon.com>
2014-10-15 13:38:23 -0400

    Disable this proxy for now until next version of webdriver that uses GSON parser exclusevely'

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/grid/servlets/ProxyStatusJsonServlet.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/grid/servlets/ProxyStatusJsonServlet.java
@@ -1,9 +1,7 @@
 package com.groupon.seleniumgridextras.grid.servlets;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.openqa.grid.common.exception.GridException;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import org.openqa.grid.internal.ProxySet;
 import org.openqa.grid.internal.Registry;
 import org.openqa.grid.internal.RemoteProxy;
@@ -12,7 +10,9 @@ import org.openqa.grid.web.servlet.RegistryBasedServlet;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -20,86 +20,85 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ProxyStatusJsonServlet extends RegistryBasedServlet {
 
-  private static final long serialVersionUID = -1975392591408983229L;
+    private static final long serialVersionUID = -1975392591408983229L;
 
-  public ProxyStatusJsonServlet() {
-    this(null);
-  }
-
-  public ProxyStatusJsonServlet(Registry registry) {
-    super(registry);
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    doPost(req, resp);
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    process(req, resp);
-
-  }
-
-  protected void process(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-    response.setContentType("application/json");
-    response.setCharacterEncoding("UTF-8");
-    response.setStatus(200);
-    JSONObject res;
-    try {
-      res = getResponse();
-      response.getWriter().print(res.toString(4));
-      response.getWriter().close();
-    } catch (JSONException e) {
-      throw new GridException(e.getMessage());
+    public ProxyStatusJsonServlet() {
+        this(null);
     }
 
-  }
-
-  private JSONObject getResponse() throws IOException, JSONException {
-    JSONObject requestJSON = new JSONObject();
-    ProxySet proxies = this.getRegistry().getAllProxies();
-    Iterator<RemoteProxy> iterator = proxies.iterator();
-    JSONArray Proxies = new JSONArray();
-    JSONArray ProxyStatus = new JSONArray();
-    while (iterator.hasNext()) {
-      RemoteProxy eachProxy = iterator.next();
-      Iterator<TestSlot> proxyIterator = eachProxy.getTestSlots().iterator();
-      while (proxyIterator.hasNext()) {
-        JSONObject TestMachine = new JSONObject();
-        TestSlot eachSlot = proxyIterator.next();
-        TestMachine.put("browserName", eachSlot.getCapabilities().get("browserName").toString());
-
-        String version = "";
-        if (eachSlot.getCapabilities().containsKey("version")) {
-          version = eachSlot.getCapabilities().get("version").toString();
-        }
-
-        TestMachine.put("version", version);
-
-        TestMachine.put("session", "");
-
-        if (eachSlot.getSession() != null) {
-          TestMachine.put("session", eachSlot.getSession().getExternalKey().getKey());
-        }
-
-        URL host = eachProxy.getRemoteHost();
-        String hostName = host.getHost();
-        TestMachine.put("host", hostName);
-
-        ProxyStatus.put(TestMachine);
-      }
-
-      Proxies.put(eachProxy.getOriginalRegistrationRequest().getAssociatedJSON());
+    public ProxyStatusJsonServlet(Registry registry) {
+        super(registry);
     }
-    requestJSON.put("Proxies", ProxyStatus);
-    requestJSON.put("TotalProxies", Proxies);
 
-    return requestJSON;
-  }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        process(req, resp);
+
+    }
+
+    protected void process(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(200);
+        response.getWriter().print(new GsonBuilder().setPrettyPrinting().create()
+                .toJson(getResponse()));
+        response.getWriter().close();
+
+
+    }
+
+    private Map getResponse() {
+        Map error = new HashMap();
+
+        error.put("message", "This servlete is currently disabled and will be depricated soon.");
+
+        return error;
+
+//        ProxySet proxies = this.getRegistry().getAllProxies();
+//        Iterator<RemoteProxy> iterator = proxies.iterator();
+//
+//        while (iterator.hasNext()) {
+//            RemoteProxy currentProxy = iterator.next();
+//            Iterator<TestSlot> proxyIterator = currentProxy.getTestSlots().iterator();
+//            while (proxyIterator.hasNext()) {
+//                Map testMachine = new HashMap();
+//                TestSlot currentTestSlot = proxyIterator.next();
+//                testMachine.put("browserName", currentTestSlot.getCapabilities().get("browserName").toString());
+//
+//                String version = "";
+//                if (currentTestSlot.getCapabilities().containsKey("version")) {
+//                    version = currentTestSlot.getCapabilities().get("version").toString();
+//                }
+//
+//                testMachine.put("version", version);
+//
+//                testMachine.put("session", "");
+//
+//                if (currentTestSlot.getSession() != null) {
+//                    testMachine.put("session", currentTestSlot.getSession().getExternalKey().getKey());
+//                }
+//
+//
+//                testMachine.put("host", currentProxy.getRemoteHost().getHost());
+//
+//
+//            }
+//
+//            Proxies.put(currentProxy.getOriginalRegistrationRequest().getAssociatedJSON());
+//        }
+//        requestJSON.put("Proxies", ProxyStatus);
+//        requestJSON.put("TotalProxies", Proxies);
+//
+//        return requestJSON;
+    }
 
 }
 
