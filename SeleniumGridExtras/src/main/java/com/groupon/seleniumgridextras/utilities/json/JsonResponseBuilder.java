9fecf4b962899717c9b4e48dd6485ce47a26bf87
Dima Kovalenko <dima@groupon.com>
2014-10-21 09:53:14 -0400

    white space changes

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/json/JsonResponseBuilder.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/json/JsonResponseBuilder.java
@@ -46,145 +46,153 @@ import java.util.Map;
 
 public class JsonResponseBuilder {
 
-  private JsonObject keyDescriptions;
-  private JsonObject keyValues;
+    private JsonObject keyDescriptions;
+    private JsonObject keyValues;
 
-  public JsonResponseBuilder() {
-    keyDescriptions = new JsonObject();
-    keyDescriptions.addProperty(JsonCodec.ERROR, "Error received during execution of command");
-    keyDescriptions.addProperty(JsonCodec.EXIT_CODE, "Exit code for operation");
-    keyDescriptions.addProperty(JsonCodec.OUT, "All of the StandardOut received from the system");
-    clearValues();
-  }
-
-  private void clearValues() {
-
-    keyValues = new JsonObject();
-
-    JsonArray out = new JsonArray();
-    JsonArray error = new JsonArray();
-
-    addKeyValues(JsonCodec.EXIT_CODE, 0);
-    addKeyValues(JsonCodec.OUT, out);
-    addKeyValues(JsonCodec.ERROR, error);
-  }
-
-  public void addKeyDescriptions(String key, String description) {
-    keyDescriptions.addProperty(key, description);
-    addKeyValues(key, "");
-  }
-
-  public void addKeyValues(String key, String value) {
-    addKeyValues(key, value, true);
-  }
-
-  public void addKeyValues(String key, String value, Boolean splitLineToArray) {
-    checkIfKeyDescriptionExist(key);
-    JsonArray valueArray = new JsonArray();
-    if (splitLineToArray) {
-      valueArray = convertLineToArray(value);
-    } else {
-      valueArray.add(new JsonPrimitive(value));
+    public JsonResponseBuilder() {
+        keyDescriptions = new JsonObject();
+        keyDescriptions.addProperty(JsonCodec.ERROR, "Error received during execution of command");
+        keyDescriptions.addProperty(JsonCodec.EXIT_CODE, "Exit code for operation");
+        keyDescriptions.addProperty(JsonCodec.OUT, "All of the StandardOut received from the system");
+        clearValues();
     }
 
-    if (key.equals(JsonCodec.OUT)) {
-      addKeyValues(key, valueArray);
-    } else if (key.equals(JsonCodec.ERROR)) {
-      addKeyValues(JsonCodec.EXIT_CODE, 1);
-      addKeyValues(key, valueArray);
-    } else {
-      keyValues.add(key, valueArray);
+    private void clearValues() {
+
+        keyValues = new JsonObject();
+
+        JsonArray out = new JsonArray();
+        JsonArray error = new JsonArray();
+
+        addKeyValues(JsonCodec.EXIT_CODE, 0);
+        addKeyValues(JsonCodec.OUT, out);
+        addKeyValues(JsonCodec.ERROR, error);
     }
 
-  }
-
-  public void addKeyValues(String key, Boolean value) {
-    checkIfKeyDescriptionExist(key);
-    keyValues.addProperty(key, value);
-  }
-
-  public void addKeyValues(String key, JsonArray value) {
-    checkIfKeyDescriptionExist(key);
-    keyValues.add(key, value);
-  }
-
-  public void addKeyValues(String key, JsonObject value) {
-    checkIfKeyDescriptionExist(key);
-    keyValues.add(key, value);
-  }
-
-  public void addKeyValues(String key, Map<String, String> value) {
-    checkIfKeyDescriptionExist(key);
-
-    keyValues.add(key, hashMapToJsonObject(value));
-  }
-
-  public void addKeyValues(String key, int value) {
-    checkIfKeyDescriptionExist(key);
-    keyValues.addProperty(key, value);
-  }
-
-  private JsonObject hashMapToJsonObject(Map<String, String> hashMap){
-    JsonObject hashToJsonObjectConverter = new JsonObject();
-    for (String hashKey : hashMap.keySet()){
-      hashToJsonObjectConverter.addProperty(hashKey, hashMap.get(hashKey));
+    public void addKeyDescriptions(String key, String description) {
+        keyDescriptions.addProperty(key, description);
+        addKeyValues(key, "");
     }
 
-    return hashToJsonObjectConverter;
-  }
-
-  public void addListOfHashes(String key, List<Map<String, String>> value) {
-    checkIfKeyDescriptionExist(key);
-
-    JsonArray valueArray = new JsonArray();
-    for (Map<String, String> hashMap : value){
-      valueArray.add(hashMapToJsonObject(hashMap));
+    public void addKeyValues(String key, String value) {
+        addKeyValues(key, value, true);
     }
-    keyValues.add(key, valueArray);
-  }
 
-  public void addKeyValues(String key, List<String> value) {
-    checkIfKeyDescriptionExist(key);
+    public void addKeyValues(String key, String value, Boolean splitLineToArray) {
+        checkIfKeyDescriptionExist(key);
+        JsonArray valueArray = new JsonArray();
+        if (splitLineToArray) {
+            valueArray = convertLineToArray(value);
+        } else {
+            valueArray.add(new JsonPrimitive(value));
+        }
 
-    JsonArray valueArray = new JsonArray();
-    for (String item : value){
-      valueArray.add(new JsonPrimitive(item));
+        if (key.equals(JsonCodec.OUT)) {
+            addKeyValues(key, valueArray);
+        } else if (key.equals(JsonCodec.ERROR)) {
+            addKeyValues(JsonCodec.EXIT_CODE, 1);
+            addKeyValues(key, valueArray);
+        } else {
+            keyValues.add(key, valueArray);
+        }
+
     }
-    keyValues.add(key, valueArray);
-  }
 
-  public String toString() {
-    String values = keyValues.toString();
-    clearValues();
-    return values;
-  }
-
-  public JsonObject getJson() {
-    JsonObject values = keyValues;
-    clearValues();
-    return values;
-  }
-
-  public JsonObject getKeyDescriptions() {
-    return keyDescriptions;
-  }
-
-  private void checkIfKeyDescriptionExist(String key) {
-    if (!keyDescriptions.has(key)) {
-      throw new RuntimeException(
-          "You cannot add an entry to Json Response without adding description for it first");
+    public void addKeyValues(String key, Boolean value) {
+        checkIfKeyDescriptionExist(key);
+        keyValues.addProperty(key, value);
     }
-  }
 
-  private JsonArray convertLineToArray(String input) {
-    JsonArray output = new JsonArray();
-
-    String stdOutLines[] = input.split("\n");
-    for (String line : stdOutLines) {
-      output.add(new JsonPrimitive(line));
+    public void addKeyValues(String key, JsonArray value) {
+        checkIfKeyDescriptionExist(key);
+        keyValues.add(key, value);
     }
-    return output;
-  }
+
+    public void addKeyValues(String key, JsonObject value) {
+        checkIfKeyDescriptionExist(key);
+        keyValues.add(key, value);
+    }
+
+    public void addKeyValues(String key, Map<String, String> value) {
+        checkIfKeyDescriptionExist(key);
+
+        keyValues.add(key, hashMapToJsonObject(value));
+    }
+
+    public void addNestedMapValues(String key, Map<String, List> value) {
+        checkIfKeyDescriptionExist(key);
+        keyValues.add(key, JsonParserWrapper.toJsonObject(value));
+    }
+
+    public void addKeyValues(String key, int value) {
+        checkIfKeyDescriptionExist(key);
+        keyValues.addProperty(key, value);
+    }
+
+    @Deprecated
+    private JsonObject hashMapToJsonObject(Map<String, String> hashMap) {
+        //TODO replace this with JsonParserWrapper.toJsonObject(); but need to test how the api will change completely
+        //That is some Arrays will become strings etc..
+        JsonObject hashToJsonObjectConverter = new JsonObject();
+        for (String hashKey : hashMap.keySet()) {
+            hashToJsonObjectConverter.addProperty(hashKey, hashMap.get(hashKey));
+        }
+
+        return hashToJsonObjectConverter;
+    }
+
+    public void addListOfHashes(String key, List<Map<String, String>> value) {
+        checkIfKeyDescriptionExist(key);
+
+        JsonArray valueArray = new JsonArray();
+        for (Map<String, String> hashMap : value) {
+            valueArray.add(hashMapToJsonObject(hashMap));
+        }
+        keyValues.add(key, valueArray);
+    }
+
+    public void addKeyValues(String key, List<String> value) {
+        checkIfKeyDescriptionExist(key);
+
+        JsonArray valueArray = new JsonArray();
+        for (String item : value) {
+            valueArray.add(new JsonPrimitive(item));
+        }
+        keyValues.add(key, valueArray);
+    }
+
+    public String toString() {
+        String values = keyValues.toString();
+        clearValues();
+        return values;
+    }
+
+    public JsonObject getJson() {
+        JsonObject values = keyValues;
+        clearValues();
+        return values;
+    }
+
+    public JsonObject getKeyDescriptions() {
+        return keyDescriptions;
+    }
+
+    private void checkIfKeyDescriptionExist(String key) {
+        if (!keyDescriptions.has(key)) {
+            throw new RuntimeException(
+                    "You cannot add an entry to Json Response without adding description for it first");
+        }
+    }
+
+    private JsonArray convertLineToArray(String input) {
+        JsonArray output = new JsonArray();
+
+        String stdOutLines[] = input.split("\n");
+        for (String line : stdOutLines) {
+            output.add(new JsonPrimitive(line));
+        }
+        return output;
+    }
 
 
 }
