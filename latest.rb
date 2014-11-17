8bd05a15d60b29adeaaef89b11a7eb0c25d2a46d
Valdis Victor Vitayaudom <victorv@groupon.com>
2014-11-17 16:54:52 -0600

    create ruby script to download latest release from https://github.com/groupon/Selenium-Grid-Extras/releases and update symlink SeleniumGridExtras-jar-with-dependencies.jar

---
+++ latest.rb
@@ -0,0 +1,13 @@
+#!/usr/local/bin/ruby
+require 'json'
+require 'fileutils'
+
+# assuming latest is first tag entry
+latest_json = JSON.parse(`curl -s https://api.github.com/repos/groupon/Selenium-Grid-Extras/releases`)[0]
+
+latest_json["assets"].each do |asset|
+  if asset["content_type"] == "application/java-archive"
+    system "curl -sL -o #{asset["name"]} #{asset["browser_download_url"]}" unless File.exist?(asset["name"])
+    FileUtils.ln_s asset["name"], 'SeleniumGridExtras-jar-with-dependencies.jar', :force => true if asset["name"] =~ /SeleniumGridExtras-(.*)-jar-with-dependencies.jar/
+  end
+end
