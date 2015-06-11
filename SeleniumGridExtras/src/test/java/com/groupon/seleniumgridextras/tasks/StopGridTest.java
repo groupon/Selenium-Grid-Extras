3ff741e8bb7f2b713ff686e4333875394239601f
Dima Kovalenko <dima@groupon.com>
2015-06-11 16:58:35 -0400

    Making the test smarter, check if port 5555 is occupied, based on that use appropriate assertion

--- SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/tasks/StopGridTest.java
+++ SeleniumGridExtras/src/test/java/com/groupon/seleniumgridextras/tasks/StopGridTest.java
@@ -1,8 +1,10 @@
 package com.groupon.seleniumgridextras.tasks;
 
+import com.google.gson.JsonObject;
+import com.groupon.seleniumgridextras.PortChecker;
 import com.groupon.seleniumgridextras.config.RuntimeConfig;
+import com.groupon.seleniumgridextras.utilities.json.JsonCodec;
 import com.sun.net.httpserver.HttpServer;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,36 +16,43 @@ import static org.junit.Assert.assertTrue;
 
 public class StopGridTest {
 
-  private StopGrid task;
-  private HttpServer server;
-  private final int port = 9999;
+    private StopGrid task;
+    private HttpServer server;
+    private final int port = 9999;
 
 
-  @Before
-  public void setUp() throws Exception {
-    task = new StopGrid();
-    server = HttpServer.create(new InetSocketAddress(port), 0);
-    server.setExecutor(null);
-    server.start();
-  }
+    @Before
+    public void setUp() throws Exception {
+        task = new StopGrid();
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.setExecutor(null);
+        server.start();
+    }
 
-  @After
-  public void tearDown() throws Exception {
-    server.stop(0);
-  }
+    @After
+    public void tearDown() throws Exception {
+        server.stop(0);
+    }
 
-  @Test
-  public void testGetWindowsCommand() throws Exception {
-    assertTrue(task.getWindowsCommand("5555").contains("taskkill -F -IM "));
-  }
+    @Test
+    public void testGetWindowsCommand() throws Exception {
 
-  @Test
-  public void testGetLinuxCommand() throws Exception {
-    String expected = "kill -9 " + RuntimeConfig.getOS().getCurrentPid();
-    assertEquals(expected, task.getLinuxCommand(port));
+        JsonObject portInfo = PortChecker.getParsedPortInfo("5555");
 
-    final String portToNeverBeFound = "4444444444444444444444444444444";
-    assertEquals("", task.getLinuxCommand(portToNeverBeFound));
-  }
+        if (portInfo.has(JsonCodec.OS.PID)) {
+            assertEquals("", task.getWindowsCommand());
+        } else {
+            assertTrue(task.getWindowsCommand().contains("taskkill -F -IM "));
+        }
+    }
+
+    @Test
+    public void testGetLinuxCommand() throws Exception {
+        String expected = "kill -9 " + RuntimeConfig.getOS().getCurrentPid();
+        assertEquals(expected, task.getLinuxCommand(port));
+
+        final String portToNeverBeFound = "4444444444444444444444444444444";
+        assertEquals("", task.getLinuxCommand(portToNeverBeFound));
+    }
 
 }
