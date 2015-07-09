5e2c649a36a6ea931403a73f7351537ab2199140
Julien LAMY <jlamy.externe@pagesjaunes.fr>
2015-07-09 15:18:52 +0200

    Adding phantomJs capabilities and upgrade selenium to 2.46.0

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/capabilities/Capability.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/capabilities/Capability.java
@@ -1,144 +1,144 @@
 package com.groupon.seleniumgridextras.config.capabilities;
 
-
-import org.apache.log4j.Logger;
-
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 public abstract class Capability extends HashMap {
 
-  private static final String MAX_INSTANCES = "maxInstances";
-  private static final String SELENIUM_PROTOCOL = "seleniumProtocol";
-  private static final String VERSION = "version";
-  private static final String PLATFORM = "platform";
-  private static final String BROWSER_NAME = "browserName";
-  private static Logger logger = Logger.getLogger(Capability.class);
+    private static final String MAX_INSTANCES = "maxInstances";
 
-  public abstract String getWebDriverClass();
+    private static final String SELENIUM_PROTOCOL = "seleniumProtocol";
 
-  public Capability() {
-    this.put(MAX_INSTANCES, 3);
-    this.put(SELENIUM_PROTOCOL, "WebDriver");
-    setBrowser(getWDStyleName());
-  }
+    private static final String VERSION = "version";
 
-  public String getBrowserVersion() {
-    return String.valueOf(this.get(VERSION));
-  }
+    private static final String PLATFORM = "platform";
 
-  public void setBrowserVersion(String browserVersion) {
-    this.put(VERSION, browserVersion);
-  }
+    private static final String BROWSER_NAME = "browserName";
 
+    private static Logger logger = Logger.getLogger(Capability.class);
 
-  public String getMaxInstances(){
-    return String.valueOf(this.get(MAX_INSTANCES));
-  }
+    public abstract String getWebDriverClass();
 
-  public void setMaxInstances(int maxInstances){
-    this.put(MAX_INSTANCES, maxInstances);
-  }
-
-  public void setPlatform(String platform) {
-    this.put(PLATFORM, platform);
-  }
-
-  protected void setBrowser(String browser) {
-    this.put(BROWSER_NAME, browser);
-  }
-
-  public String getBrowser() {
-    return String.valueOf(this.get(BROWSER_NAME));
-  }
-
-  public static Capability getCapabilityFor(String browserName, Map capabilityMap) {
-    Capability cap = getCapabilityFor(browserName);
-    for (Object capabilityKey : capabilityMap.keySet()) {
-
-      Object value = capabilityMap.get(capabilityKey);
-      if (value instanceof Number){
-        //GSON library always converts ints into doubles :(
-        value = ((Number) value).intValue();
-      }
-
-      if (capabilityKey.equals(VERSION)){
-        //Explicitly convert the Version of the browser into a string instead of an int or double.
-        //If we don't do that, then the grid cannot find the provided version :(
-        value = String.valueOf(value);
-      }
-
-      cap.put(capabilityKey, value);
+    public Capability() {
+        this.put(MAX_INSTANCES, 3);
+        this.put(SELENIUM_PROTOCOL, "WebDriver");
+        setBrowser(getWDStyleName());
     }
 
+    public String getBrowserVersion() {
+        return String.valueOf(this.get(VERSION));
+    }
 
-    return cap;
-  }
+    public void setBrowserVersion(final String browserVersion) {
+        this.put(VERSION, browserVersion);
+    }
 
+    public String getMaxInstances() {
+        return String.valueOf(this.get(MAX_INSTANCES));
+    }
 
-  public static Capability getCapabilityFor(String browserName) {
+    public void setMaxInstances(final int maxInstances) {
+        this.put(MAX_INSTANCES, maxInstances);
+    }
 
-    for (Map.Entry<Class, String> entry : Capability.getSupportedCapabilities().entrySet()) {
+    public void setPlatform(final String platform) {
+        this.put(PLATFORM, platform);
+    }
 
-      Class<Capability> key = entry.getKey();
-      String value = entry.getValue();
+    protected void setBrowser(final String browser) {
+        this.put(BROWSER_NAME, browser);
+    }
 
-      if (value.equals(browserName)) {
-        try {
-          return key.newInstance();
-        } catch (Exception e) {
-          logger.error("Can't load capability from file, exiting with 1");
-          logger.equals(e);
-          e.printStackTrace();
-          System.exit(1);
+    public String getBrowser() {
+        return String.valueOf(this.get(BROWSER_NAME));
+    }
+
+    public static Capability getCapabilityFor(final String browserName, final Map capabilityMap) {
+        Capability cap = getCapabilityFor(browserName);
+        for (Object capabilityKey : capabilityMap.keySet()) {
+
+            Object value = capabilityMap.get(capabilityKey);
+            if (value instanceof Number) {
+                //GSON library always converts ints into doubles :(
+                value = ((Number) value).intValue();
+            }
+
+            if (capabilityKey.equals(VERSION)) {
+                //Explicitly convert the Version of the browser into a string instead of an int or double.
+                //If we don't do that, then the grid cannot find the provided version :(
+                value = String.valueOf(value);
+            }
+
+            cap.put(capabilityKey, value);
         }
-      }
 
+        return cap;
     }
 
-    return null;
-  }
+    public static Capability getCapabilityFor(final String browserName) {
 
-  public static Map<Class, String> getSupportedWebCapabilities() {
-    Map<Class, String> capabilityHash = new LinkedHashMap<Class, String>();
+        for (Map.Entry<Class, String> entry : Capability.getSupportedCapabilities().entrySet()) {
 
-    capabilityHash.put(Firefox.class, BrowserType.FIREFOX);
-    capabilityHash.put(InternetExplorer.class, BrowserType.IE);
-    capabilityHash.put(Chrome.class, BrowserType.CHROME);
-    capabilityHash.put(Safari.class, BrowserType.SAFARI);
+            Class<Capability> key = entry.getKey();
+            String value = entry.getValue();
 
-    return capabilityHash;
-  }
+            if (value.equals(browserName)) {
+                try {
+                    return key.newInstance();
+                } catch (Exception e) {
+                    logger.error("Can't load capability from file, exiting with 1");
+                    logger.equals(e);
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
 
-  public static Map<Class, String> getSupportedAppiumCapabilities() {
-    Map<Class, String> capabilityHash = new LinkedHashMap<Class, String>();
+        }
 
-    capabilityHash.put(Android.class, BrowserType.ANDROID);
-    capabilityHash.put(Chrome.class, BrowserType.CHROME);
-    capabilityHash.put(Chromium.class, BrowserType.CHROMIUM);
-    capabilityHash.put(Browser.class, BrowserType.BROWSER);
-    capabilityHash.put(IPhone.class, BrowserType.IPHONE);
-    capabilityHash.put(IPad.class, BrowserType.IPAD);
-    capabilityHash.put(Safari.class, BrowserType.SAFARI);
+        return null;
+    }
 
-    return capabilityHash;
-  }
+    public static Map<Class, String> getSupportedWebCapabilities() {
+        Map<Class, String> capabilityHash = new LinkedHashMap<Class, String>();
 
-  public static Map<Class, String> getSupportedCapabilities() {
-      Map<Class, String> capabilityHash = new HashMap<Class, String>();
+        capabilityHash.put(Firefox.class, BrowserType.FIREFOX);
+        capabilityHash.put(PhantomJs.class, BrowserType.PHANTOMJS);
+        capabilityHash.put(InternetExplorer.class, BrowserType.IE);
+        capabilityHash.put(Chrome.class, BrowserType.CHROME);
+        capabilityHash.put(Safari.class, BrowserType.SAFARI);
 
-      capabilityHash.putAll(getSupportedWebCapabilities());
-      capabilityHash.putAll(getSupportedAppiumCapabilities());
+        return capabilityHash;
+    }
 
-      return capabilityHash;
-  }
+    public static Map<Class, String> getSupportedAppiumCapabilities() {
+        Map<Class, String> capabilityHash = new LinkedHashMap<Class, String>();
 
-  public String getWDStyleName() {
-    return Capability.getSupportedCapabilities().get(this.getClass());
-  }
+        capabilityHash.put(Android.class, BrowserType.ANDROID);
+        capabilityHash.put(Chrome.class, BrowserType.CHROME);
+        capabilityHash.put(Chromium.class, BrowserType.CHROMIUM);
+        capabilityHash.put(Browser.class, BrowserType.BROWSER);
+        capabilityHash.put(IPhone.class, BrowserType.IPHONE);
+        capabilityHash.put(IPad.class, BrowserType.IPAD);
+        capabilityHash.put(Safari.class, BrowserType.SAFARI);
 
-  public abstract String getIcon();
+        return capabilityHash;
+    }
+
+    public static Map<Class, String> getSupportedCapabilities() {
+        Map<Class, String> capabilityHash = new HashMap<Class, String>();
+
+        capabilityHash.putAll(getSupportedWebCapabilities());
+        capabilityHash.putAll(getSupportedAppiumCapabilities());
+
+        return capabilityHash;
+    }
+
+    public String getWDStyleName() {
+        return Capability.getSupportedCapabilities().get(this.getClass());
+    }
+
+    public abstract String getIcon();
 }
-
-
