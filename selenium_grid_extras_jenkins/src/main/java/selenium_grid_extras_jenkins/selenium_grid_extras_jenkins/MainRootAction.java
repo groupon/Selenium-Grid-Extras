97366cb9070b45203e7d6a622fba47c90abbf399
Dima Kovalenko <dima@groupon.com>
2015-07-24 14:47:17 -0400

    Smarter restarting of the nodes when the test no longer runs

--- selenium_grid_extras_jenkins/src/main/java/selenium_grid_extras_jenkins/selenium_grid_extras_jenkins/MainRootAction.java
+++ selenium_grid_extras_jenkins/src/main/java/selenium_grid_extras_jenkins/selenium_grid_extras_jenkins/MainRootAction.java
@@ -5,12 +5,15 @@ import hudson.Extension;
 import hudson.Plugin;
 import hudson.model.Action;
 import hudson.model.RootAction;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
 @Extension
 
 public class MainRootAction extends Plugin implements Action, RootAction {
 
     public static final String GRID_EXTRAS_ENDPOINT = "/gridExtras";
+    public static final String HUB_URL = "http://localhost";
 
     @Override
     public String getIconFileName() {
@@ -25,5 +28,20 @@ public class MainRootAction extends Plugin implements Action, RootAction {
     @Override
     public String getUrlName() {
         return GRID_EXTRAS_ENDPOINT;
+    }
+
+    @Override
+    public void start(){
+        System.out.println("Start");
+    }
+
+    public String doNodes(StaplerRequest req, StaplerResponse rsp) {
+        rsp.setContentType("application/json");
+        return "{}";
+    }
+
+    public String doHubConsole(StaplerRequest req, StaplerResponse rsp){
+        rsp.setContentType("text/html");
+        return "<html><body><h1>hi</h1></body></html>";
     }
 }
