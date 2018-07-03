52ceb775664e21ce191d6f550087d58c84d5f6d3
damencho <damencho@jitsi.org>
2018-07-03 17:57:34 -0500

    Changes from deprecated xuggler to humble-video.

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/threads/video/VideoRecorderCallable.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/utilities/threads/video/VideoRecorderCallable.java
@@ -5,11 +5,9 @@ import com.groupon.seleniumgridextras.config.RuntimeConfig;
 import com.groupon.seleniumgridextras.utilities.ScreenshotUtility;
 import com.groupon.seleniumgridextras.utilities.TimeStampUtility;
 import com.groupon.seleniumgridextras.videorecording.ImageProcessor;
-import com.xuggle.mediatool.IMediaWriter;
-import com.xuggle.mediatool.ToolFactory;
-import com.xuggle.xuggler.ICodec;
-import com.xuggle.xuggler.IRational;
 
+import io.humble.video.*;
+import io.humble.video.awt.*;
 import org.apache.commons.io.comparator.LastModifiedFileComparator;
 import org.apache.log4j.Logger;
 
@@ -19,7 +17,6 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 
 
 public class VideoRecorderCallable implements Callable {
@@ -38,10 +35,10 @@ public class VideoRecorderCallable implements Callable {
 
 
     final private static
-    IRational
-            FRAME_RATE =
-            IRational.make(RuntimeConfig.getConfig().getVideoRecording().getFrames(),
-                    RuntimeConfig.getConfig().getVideoRecording().getSecondsPerFrame());
+    Rational
+        FRAME_RATE =
+        Rational.make(RuntimeConfig.getConfig().getVideoRecording().getSecondsPerFrame(),
+                RuntimeConfig.getConfig().getVideoRecording().getFrames());
     private static Dimension dimension;
 
 
@@ -78,55 +75,159 @@ public class VideoRecorderCallable implements Callable {
         // Note we're writing to a temp file.  This is to prevent it from being
         // downloaded while we're mid-write.
         final File tempFile = new File(outputDir, sessionId + ".temp.mp4");
-        final
-        IMediaWriter
-                writer =
-                ToolFactory.makeWriter(tempFile.getAbsolutePath());
+
+        /** First we create a muxer using the passed in filename and formatname if given. */
+        final Muxer muxer = Muxer.make(tempFile.getAbsolutePath(), null, /*formatname*/null);
+
+        /** Now, we need to decide what type of codec to use to encode video. Muxers
+         * have limited sets of codecs they can use. We're going to pick the first one that
+         * works, or if the user supplied a codec name, we're going to force-fit that
+         * in instead.
+         */
+        final MuxerFormat format = muxer.getFormat();
+
+        final Codec codec = Codec.findEncodingCodec(Codec.ID.CODEC_ID_H264);
 
         // We tell it we're going to add one video stream, with id 0,
         // at position 0, and that it will have a fixed frame rate of
         // FRAME_RATE.
-        writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264,
-                FRAME_RATE,
-                screenBounds.width, screenBounds.height);
+
+        /**
+         * Now that we know what codec, we need to create an encoder
+         */
+        Encoder encoder = Encoder.make(codec);
+
+        /**
+         * Video encoders need to know at a minimum:
+         *   width
+         *   height
+         *   pixel format
+         * Some also need to know frame-rate (older codecs that had a fixed rate at which video files could
+         * be written needed this). There are many other options you can set on an encoder, but we're
+         * going to keep it simpler here.
+         */
+        encoder.setWidth(screenBounds.width);
+        encoder.setHeight(screenBounds.height);
+        // We are going to use 420P as the format because that's what most video formats these days use
+        final PixelFormat.Type pixelformat = PixelFormat.Type.PIX_FMT_YUV420P;
+        encoder.setPixelFormat(pixelformat);
+        encoder.setTimeBase(FRAME_RATE);
+
+        /** An annoynace of some formats is that they need global (rather than per-stream) headers,
+         * and in that case you have to tell the encoder. And since Encoders are decoupled from
+         * Muxers, there is no easy way to know this beyond
+         */
+        if (format.getFlag(MuxerFormat.Flag.GLOBAL_HEADER))
+            encoder.setFlag(Encoder.Flag.FLAG_GLOBAL_HEADER, true);
+
+        /** Open the encoder. */
+        encoder.open(null, null);
+
+        /** Add this stream to the muxer. */
+        muxer.addNewStream(encoder);
+
+        /** And open the muxer for business. */
+        muxer.open(null, null);
+
+        /** Next, we need to make sure we have the right MediaPicture format objects
+         * to encode data with. Java (and most on-screen graphics programs) use some
+         * variant of Red-Green-Blue image encoding (a.k.a. RGB or BGR). Most video
+         * codecs use some variant of YCrCb formatting. So we're going to have to
+         * convert. To do that, we'll introduce a MediaPictureConverter object later. object.
+         */
+        MediaPictureConverter converter = null;
+        final MediaPicture picture = MediaPicture.make(
+            encoder.getWidth(),
+            encoder.getHeight(),
+            pixelformat);
+        picture.setTimeBase(FRAME_RATE);
 
         logger
-                .info("Starting video recording for session " + getSessionId() + " to " + outputDir
-                        .getAbsolutePath());
+            .info("Starting video recording for session " + getSessionId() + " to " + outputDir
+                .getAbsolutePath());
 
+        final MediaPacket packet = MediaPacket.make();
         try {
-            int imageFrame = 1;
-            long startTime = System.nanoTime();
-            addTitleFrame(writer);
+            int imageFrame = 0;
+
+            {
+                BufferedImage titleFrame
+                    = ImageProcessor.createTitleFrame(
+                    dimension,
+                    BufferedImage.TYPE_3BYTE_BGR,
+                    "Session :" + this.sessionId,
+                    "Host :" + RuntimeConfig.getOS().getHostName() + " ("
+                        + RuntimeConfig.getHostIp() + ")",
+                    getTimestamp().toString());
+                if (converter == null)
+                    converter = MediaPictureConverterFactory
+                        .createConverter(titleFrame, picture);
+                converter.toPicture(picture, titleFrame, imageFrame++);
+
+                do
+                {
+                    encoder.encode(packet, picture);
+                    if (packet.isComplete())
+                        muxer.write(packet, false);
+                }
+                while (packet.isComplete());
+                try
+                {
+                    Thread.sleep(2);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
 
             while (stopActionNotCalled() && idleTimeoutNotReached()) {
 
                 // take the screen shot
                 BufferedImage
-                        screenshot =
-                        ScreenshotUtility.getResizedScreenshot(dimension.width, dimension.height);
+                    screenshot =
+                    ScreenshotUtility.getResizedScreenshot(dimension.width, dimension.height);
 
                 screenshot = ImageProcessor.addTextCaption(screenshot,
-                        "Session: " + this.sessionId,
-                        "Host: " + this.nodeName,
-                        "Timestamp: " + getTimestamp().toString(),
-                        this.lastAction
+                    "Session: " + this.sessionId,
+                    "Host: " + this.nodeName,
+                    "Timestamp: " + getTimestamp().toString(),
+                    this.lastAction
                 );
 
                 // convert to the right image type
                 BufferedImage bgrScreen = convertToType(screenshot,
-                        BufferedImage.TYPE_3BYTE_BGR);
+                    BufferedImage.TYPE_3BYTE_BGR);
 
                 // encode the image
-                writer.encodeVideo(0, bgrScreen,
-                        System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+                /** This is LIKELY not in YUV420P format, so we're going to convert it using some handy utilities. */
+                if (converter == null)
+                    converter = MediaPictureConverterFactory.createConverter(bgrScreen, picture);
+                converter.toPicture(picture, bgrScreen, imageFrame++);
+
+                do {
+                    encoder.encode(packet, picture);
+                    if (packet.isComplete())
+                        muxer.write(packet, false);
+                } while (packet.isComplete());
 
                 // sleep for framerate milliseconds
-                Thread.sleep((long) (1000 / FRAME_RATE.getDouble()));
+                Thread.sleep((long) (1000 * FRAME_RATE.getDouble()));
 
             }
         } finally {
-            writer.close();
+            /** Encoders, like decoders, sometimes cache pictures so it can do the right key-frame optimizations.
+             * So, they need to be flushed as well. As with the decoders, the convention is to pass in a null
+             * input until the output is not complete.
+             */
+            do {
+                encoder.encode(packet, null);
+                if (packet.isComplete())
+                    muxer.write(packet,  false);
+            } while (packet.isComplete());
+
+            /** Finally, let's clean up after ourselves. */
+            muxer.close();
 
             // Now, rename our temporary file to the final filename, so that the downloaders can detect it
             final File finalFile = new File(outputDir, sessionId + ".mp4");
@@ -146,24 +247,6 @@ public class VideoRecorderCallable implements Callable {
         return getSessionId();
     }
 
-    protected void addTitleFrame(IMediaWriter writer) {
-        writer.encodeVideo(0,
-                ImageProcessor
-                        .createTitleFrame(dimension, BufferedImage.TYPE_3BYTE_BGR,
-                                "Session :" + this.sessionId,
-                                "Host :" + RuntimeConfig.getOS().getHostName() + " ("
-                                        + RuntimeConfig.getHostIp() + ")",
-                                getTimestamp().toString()),
-                0,
-                TimeUnit.NANOSECONDS);
-        try {
-            Thread.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-
     public void lastAction(String action) {
         this.lastActionTimestamp = getTimestamp();
         this.lastAction = action;
@@ -180,7 +263,7 @@ public class VideoRecorderCallable implements Callable {
     protected void setOutputDirExists(String sessionId) {
         if (!outputDir.exists()) {
             System.out.println(
-                    "Root Video output dir does not exist, creating it here " + outputDir.getAbsolutePath());
+                "Root Video output dir does not exist, creating it here " + outputDir.getAbsolutePath());
             outputDir.mkdir();
         }
     }
@@ -217,17 +300,17 @@ public class VideoRecorderCallable implements Callable {
     protected void dynamicallySetDimension() {
         try {
             BufferedImage
-                    sample =
-                    ScreenshotUtility
-                            .getResizedScreenshot(RuntimeConfig.getConfig().getVideoRecording().getWidth(),
-                                    RuntimeConfig.getConfig().getVideoRecording().getHeight());
+                sample =
+                ScreenshotUtility
+                    .getResizedScreenshot(RuntimeConfig.getConfig().getVideoRecording().getWidth(),
+                        RuntimeConfig.getConfig().getVideoRecording().getHeight());
             dimension = new Dimension(sample.getWidth(), sample.getHeight());
         } catch (AWTException e) {
             e.printStackTrace();
             logger.equals(e);
             dimension =
-                    new Dimension(RuntimeConfig.getConfig().getVideoRecording().getWidth(),
-                            RuntimeConfig.getConfig().getVideoRecording().getHeight());
+                new Dimension(RuntimeConfig.getConfig().getVideoRecording().getWidth(),
+                    RuntimeConfig.getConfig().getVideoRecording().getHeight());
         }
 
     }
