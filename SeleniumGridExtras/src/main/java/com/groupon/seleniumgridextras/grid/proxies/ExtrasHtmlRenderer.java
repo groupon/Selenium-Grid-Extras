497010a48d3a66e22e3074580744836263a3c4c0
Conner Tolley <jim.tolley@hcahealthcare.com>
2019-05-22 17:37:01 -0500

    Add method to replace BrowserNameUtils, which doesn't exist anymore.

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/grid/proxies/ExtrasHtmlRenderer.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/grid/proxies/ExtrasHtmlRenderer.java
@@ -1,13 +1,14 @@
 package com.groupon.seleniumgridextras.grid.proxies;
 
+import com.google.common.base.Strings;
 import org.openqa.grid.internal.TestSession;
 import org.openqa.grid.internal.TestSlot;
 import org.openqa.grid.internal.utils.HtmlRenderer;
 import org.openqa.grid.selenium.proxy.DefaultRemoteProxy;
-import org.openqa.grid.web.utils.BrowserNameUtils;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.InputStream;
 import java.util.Map;
 
 public class ExtrasHtmlRenderer implements HtmlRenderer {
@@ -40,7 +41,7 @@ public class ExtrasHtmlRenderer implements HtmlRenderer {
     for (TestSlot slot : proxy.getTestSlots()) {
       TestSession session = slot.getSession();
 
-      String icon = getIcon(slot.getCapabilities());
+      String icon = getConsoleIconPath(new DesiredCapabilities(slot.getCapabilities()));
       if (icon != null) {
         builder.append("<img ");
         builder.append("src='").append(icon).append("' ");
@@ -72,8 +73,52 @@ public class ExtrasHtmlRenderer implements HtmlRenderer {
     return builder.toString();
   }
 
-  private String getIcon(Map<String, Object> capabilities) {
-    return BrowserNameUtils.getConsoleIconPath(new DesiredCapabilities(capabilities),
-        proxy.getRegistry());
+  private String getConsoleIconPath(DesiredCapabilities cap) {
+    String name = this.consoleIconName(cap);
+    String path = "org/openqa/grid/images/";
+    InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(path + name + ".png");
+    return in == null ? null : "/grid/resources/" + path + name + ".png";
+  }
+
+  private String consoleIconName(DesiredCapabilities cap) {
+    String browserString = cap.getBrowserName();
+    if (Strings.isNullOrEmpty(browserString)) {
+      return "missingBrowserName";
+    } else {
+      String ret = browserString;
+      if (!browserString.contains("iexplore") && !browserString.startsWith("*iehta")) {
+        if (!browserString.contains("firefox") && !browserString.startsWith("*chrome")) {
+          if (browserString.toLowerCase().contains("safari")) {
+            if (browserString.toLowerCase().contains("technology")) {
+              ret = "safari_technology_preview";
+            } else {
+              ret = "safari";
+            }
+          } else if (browserString.startsWith("*googlechrome")) {
+            ret = "chrome";
+          } else if (browserString.startsWith("opera")) {
+            ret = "opera";
+          } else if (browserString.toLowerCase().contains("edge")) {
+            ret = "MicrosoftEdge";
+          }
+        } else if ((cap.getVersion() == null || !cap.getVersion().toLowerCase().equals("beta")) && !cap.getBrowserName().toLowerCase().contains("beta")) {
+          if ((cap.getVersion() == null || !cap.getVersion().toLowerCase().equals("aurora")) && !cap.getBrowserName().toLowerCase().contains("aurora")) {
+            if ((cap.getVersion() == null || !cap.getVersion().toLowerCase().equals("nightly")) && !cap.getBrowserName().toLowerCase().contains("nightly")) {
+              ret = "firefox";
+            } else {
+              ret = "nightly";
+            }
+          } else {
+            ret = "aurora";
+          }
+        } else {
+          ret = "firefoxbeta";
+        }
+      } else {
+        ret = "internet explorer";
+      }
+
+      return ret.replace(" ", "_");
+    }
   }
 }
